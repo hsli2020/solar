00670fa10d7addc49b2d7f56b3dea3b4911eb91b
Hanson Li <lihsca@gmail.com>
2016-09-24 11:51:57 -0400

    private pages for only authed users

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -2,12 +2,42 @@
 
 namespace App\Controllers;
 
+use Phalcon\Mvc\Dispatcher;
 use Phalcon\Mvc\Controller;
-use Phalcon\DI\FactoryDefault as Di;
 
 class ControllerBase extends Controller
 {
     public function initialize()
     {
+    }
+
+    public function beforeExecuteRoute(Dispatcher $dispatcher)
+    {
+        $controllerName = $dispatcher->getControllerName();
+
+        // Only check permissions on private controllers
+        if ($this->isPrivate($controllerName)) {
+            // Get the current identity
+            $auth = $this->session->get('auth');
+
+            // If there is no identity available the user is redirected to user/login
+            if (!is_array($auth)) {
+                //$this->flash->notice('You don\'t have access to this module: private');
+                //$dispatcher->forward(['controller' => 'user', 'action' => 'login']);
+                $this->response->redirect("/user/login");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function isPrivate($controllerName)
+    {
+        $privateControllers = array(
+            'index',
+        );
+
+        return in_array($controllerName, $privateControllers);
     }
 }

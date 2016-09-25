212e0fe359c1b6a1427d09fb2b1ef1237e3b7711
Hanson Li <lihsca@gmail.com>
2016-09-25 18:04:18 -0400

    use America/Toronto instead of EST

--- app/utils/Utils.php
+++ app/utils/Utils.php
@@ -3,6 +3,6 @@
 function toLocaltime($timeStr)
 {
     $date = new \DateTime($timeStr, new \DateTimeZone('UTC'));
-    $date->setTimezone(new \DateTimeZone('EST'));
+    $date->setTimezone(new \DateTimeZone('America/Toronto'));
     return $date->format('Y-m-d H:i:s');
 }

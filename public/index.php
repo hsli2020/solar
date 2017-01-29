95e3b57a9e6411f2b6fca40f5db69b971c92395b
Hanson Li <lihsca@gmail.com>
2017-01-28 23:03:40 -0500

    move trace.php from app/logs/ to public/

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 define('BASE_DIR', dirname(__DIR__));
 #define('APP_DIR', BASE_DIR . '/app');
 
-include BASE_DIR . '/app/logs/trace.php';
+include BASE_DIR . '/public/trace.php';
 include BASE_DIR . "/app/bootstrap.php";
 
 $bootstrap = new Bootstrap();

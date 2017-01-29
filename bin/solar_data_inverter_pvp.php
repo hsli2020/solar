82ccc5f203b4bd9abfec916e475178b46554855d
Hanson Li <lihsca@gmail.com>
2017-01-28 21:43:31 -0500

    two new tables for data-import

---
+++ bin/solar_data_inverter_pvp.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'time',
+    'error',
+    'low_alarm',
+    'high_alarm',
+    'kw',
+    'status',
+    'fault_code_0',
+    'fault_code_1',
+    'fault_code_2',
+    'vln_a',
+    'vln_b',
+    'vln_c',
+];
---
+++ bin/solar_data_inverter_sma.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'time',
+    'error',
+    'low_alarm',
+    'high_alarm',
+    'kw',
+    'status',
+    'fault_code',
+    'volt_a',
+    'volt_b',
+    'volt_c',
+];

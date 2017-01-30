8f0993f0a676e1eec5278d21e9a4eaa8fbdfb4de
Hanson Li <lihsca@gmail.com>
2017-01-29 21:51:25 -0500

    SnapshotService

---
+++ app/services/SnapshotService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+use Phalcon\Di\Injectable;
+
+use App\Models\Snapshot;
+
+class SnapshotService extends Injectable
+{
+    public function load()
+    {
+    }
+
+    public function generate()
+    {
+    }
+}

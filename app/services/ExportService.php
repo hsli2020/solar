f50bf7479b0650102e6c2dba7c3549e46b83b4fc
Hanson Li <lihsca@gmail.com>
2017-02-02 09:25:30 -0500

    ExportService & ReportService

---
+++ app/services/ExportService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Phalcon\Di\Injectable;
+
+class ExportService extends Injectable
+{
+    public function exportMinute1($start, $end)
+    {
+    }
+
+    public function exportMinute15($start, $end)
+    {
+    }
+
+    public function exportHour1($start, $end)
+    {
+    }
+
+    public function exportDaily($start, $end)
+    {
+    }
+
+    public function exportMonthly($start, $end)
+    {
+    }
+}
---
+++ app/services/ReportService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use Phalcon\Di\Injectable;
+
+class ReportService extends Injectable
+{
+    public function genDailyReport()
+    {
+    }
+
+    public function sendDailyReport()
+    {
+    }
+
+    public function genMonthlyReport()
+    {
+    }
+
+    public function sendMonthlyReport()
+    {
+    }
+}

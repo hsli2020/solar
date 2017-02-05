ce02e9b58b2dcd3c99971f7b1497a1c089de8fab
Hanson Li <lihsca@gmail.com>
2017-02-05 14:20:25 -0500

    not finished methods in dataService

--- app/services/DataService.php
+++ app/services/DataService.php
@@ -67,4 +67,67 @@ class DataService extends Injectable
 
         return $data;
     }
+
+    public function getIRR()
+    {
+    }
+
+    public function getTMP()
+    {
+    }
+
+    public function getKW()
+    {
+    }
+
+    public function getAvgIRR()
+    {
+    }
+
+    public function getKWH()
+    {
+    }
+
+    public function getRefData($prj, $year, $month)
+    {
+        return $this->db->fetchOne("SELECT * FROM project_reference_data
+            WHERE project_id=$prj AND year=$year AND month=$month");
+    }
+
+    public function getPR($prj)
+    {
+        $site = $this->projectService->get($prj);
+
+        $DC_Nameplate_Capacity    = $site['DC_Nameplate_Capacity'];
+        $AC_Nameplate_Capacity    = $site['AC_Nameplate_Capacity'];
+
+        $Module_Power_Coefficient = $site['Module_Power_Coefficient'];
+        $Inverter_Efficiency      = $site['Inverter_Efficiency'];
+        $Transformer_Loss         = $site['Transformer_Loss'];
+        $Other_Loss               = $site['Other_Loss'];
+
+        $Avg_Irradiance_POA       = 594.816;  // avg 60 minutes
+        $Avg_Module_Temp          = 37;          // PANELT
+        $Measured_Energy          = 7483;        // sum 60 minutes
+
+        $Maximum_Theory_Output = ($Avg_Irradiance_POA / 1000) * $DC_Nameplate_Capacity;
+
+        $Temperature_Losses = ($Maximum_Theory_Output * ($Module_Power_Coefficient * (25 - $Avg_Module_Temp))) / 1000.0;
+        $Inverter_Losses = (1 - $Inverter_Efficiency) * ($Maximum_Theory_Output - $Temperature_Losses);
+
+        if (($Maximum_Theory_Output - $Temperature_Losses - $Inverter_Losses) > $AC_Nameplate_Capacity) {
+            $Inverter_Clipping_Loss = $Maximum_Theory_Output - $Temperature_Losses - $Inverter_Losses - $AC_Nameplate_Capacity;
+        } else {
+            $Inverter_Clipping_Loss = 0;
+        }
+
+        $Transformer_Losses  = ($Maximum_Theory_Output - $Temperature_Losses - $Inverter_Losses - $Inverter_Clipping_Loss) * $Transformer_Loss;
+        $Other_System_Losses = ($Maximum_Theory_Output - $Temperature_Losses - $Inverter_Losses - $Inverter_Clipping_Loss - $Transformer_Loss) * $Other_Loss;
+        $Total_Losses = ($Temperature_Losses + $Inverter_Losses + $Inverter_Clipping_Loss + $Transformer_Loss + $Other_System_Losses) / $Maximum_Theory_Output;
+        $Theoretical_Output = ($Maximum_Theory_Output - $Temperature_Losses - $Inverter_Losses - $Inverter_Clipping_Loss - $Transformer_Loss - $Other_System_Losses);
+
+        $GCS_Performance_Index = $Measured_Energy / $Theoretical_Output;
+
+        return $GCS_Performance_Index;
+    }
 }

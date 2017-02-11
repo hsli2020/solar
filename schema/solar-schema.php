f1ef1f237e0215264fc8891ce6dcad7548298bbb
Hanson Li <lihsca@gmail.com>
2017-02-11 11:23:15 -0500

    schema/solar-schema.php

---
+++ schema/solar-schema.php
@@ -0,0 +1,61 @@
+<?php
+
+solar_data_envkit = [ 'time', 'error', 'low_alarm', 'high_alarm',
+    'OAT',
+    'PANELT',
+    'IRR'
+];
+
+solar_data_genmeter = [ 'time', 'error', 'low_alarm', 'high_alarm',
+    'kva',
+    'kwh_del',
+    'kwh_rec',
+    'vln_a',
+    'vln_b',
+    'vln_c'
+];
+
+solar_data_inverter_pvp = [ 'time', 'error', 'low_alarm', 'high_alarm',
+    'kw',
+    'status',
+    'fault_code_0',
+    'fault_code_1',
+    'fault_code_2',
+    'vln_a',
+    'vln_b',
+    'vln_c',
+];
+
+solar_data_inverter_serial = [ 'time', 'error', 'low_alarm', 'high_alarm',
+    'total_kwh_del',
+    'volts_a',
+    'volts_b',
+    'volts_c',
+    'current_a',
+    'current_b',
+    'current_c',
+    'dc_input_voltage',
+    'dc_input_current',
+    'line_freq',
+    'line_kw',
+    'inverter_operating_status',
+    'inverter_fault_word_0',
+    'inverter_fault_word_1',
+    'inverter_fault_word_2',
+    'data_comm_status'
+];
+
+solar_data_inverter_sma = [ 'time', 'error', 'low_alarm', 'high_alarm',
+    'kw',
+    'status',
+    'fault_code',
+    'volt_a',
+    'volt_b',
+    'volt_c',
+];
+
+solar_data_inverter_tcp = [ 'time', 'error', 'low_alarm', 'high_alarm',
+    'dcvolts',
+    'kw',
+    'kwh'
+];

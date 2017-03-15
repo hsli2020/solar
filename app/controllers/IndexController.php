486aa0efbdd57b69f02951b75a10a4e7d55a3187
Hongshuan Li <lihsca@gmail.com>
2017-03-15 09:41:22 -0400

    dashboard as home page

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends ControllerBase
     {
         return $this->dispatcher->forward([
             'controller' => 'index',
-            'action' => 'project'
+            'action' => 'dashboard'
         ]);
     }
 

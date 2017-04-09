9141c54b502d6e0024f0775910a5985da6dd999b
Hongshuan Li <lihsca@gmail.com>
2017-04-09 10:34:40 -0400

    Revert "The old daily-report job might be still useful"
    This reverts commit 62ba422ce9c096a794ab471db392a51b86aaca26.

--- job/daily-report.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include __DIR__ . '/../public/init.php';
-
-$di = \Phalcon\Di::getDefault();
-
-$dailyReport = $di->get('dailyReportService');
-$dailyReport->generate();
-$dailyReport->send();

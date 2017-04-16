b3b2863129bcb61ab5166955992694fb2a21b79d
Hongshuan Li <lihsca@gmail.com>
2017-04-16 14:19:54 -0400

    new controller ReportController

---
+++ app/controllers/ReportController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controllers;
+
+class ReportController extends ControllerBase
+{
+    public function indexAction()
+    {
+        return $this->dispatcher->forward([
+            'controller' => 'report',
+            'action' => 'daily'
+        ]);
+    }
+
+    public function dailyAction()
+    {
+        $this->view->pageTitle = 'Daily Report';
+
+        $date = date('Ymd', strtotime('-1 day'));
+        $filename = BASE_DIR . "/app/logs/daily-report-$date.json";
+
+        if (!file_exists($filename)) {
+            return;
+        }
+
+        $json = file_get_contents($filename);
+        $report = json_decode($json, true);
+
+        $this->view->report = $report;
+    }
+
+    public function monthlyAction()
+    {
+        $this->view->pageTitle = 'Monthly Report';
+
+        $date = date('Ymd', strtotime('-1 day'));
+        $filename = BASE_DIR . "/app/logs/monthly-report-$date.json";
+
+        if (!file_exists($filename)) {
+            return;
+        }
+
+        $json = file_get_contents($filename);
+        $report = json_decode($json, true);
+
+        $this->view->report = $report;
+    }
+}

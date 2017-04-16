90a08352ca26633a74dab294e712e0a717a7408a
Hongshuan Li <lihsca@gmail.com>
2017-04-16 10:58:13 -0400

    pr-logger fix

--- job/pr-logger.php
+++ job/pr-logger.php
@@ -4,16 +4,15 @@ include __DIR__ . '/../public/init.php';
 
 $di = \Phalcon\Di::getDefault();
 
-$dataService = $di->get('dataService');
 $projectService = $di->get('projectService');
 
 $fp = fopen(BASE_DIR . '/app/logs/PR.log', 'a+');
 
 $projects = $projectService->getAll();
 foreach ($projects as $project) {
-    $id = $project['id'];
-    $name = $project['name'];
-    $PR = round($dataService->getPR($id)*100).'%';
+    $id = $project->id;
+    $name = $project->name;
+    $PR = round($project->getPR()*100).'%';
     fputs($fp, sprintf("%d  %-25s %-24s %d%%\n", $id, $name, date('Y-m-d H:i:s'), $PR));
 }
 fputs($fp, "\n");

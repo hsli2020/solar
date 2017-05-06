d0cc0fada926bfc3b96dfb5ffea893f9cc54c955
Hongshuan Li <lihsca@gmail.com>
2017-05-06 09:45:29 -0400

    daily-report execel template update

--- app/services/DailyReportService.php
+++ app/services/DailyReportService.php
@@ -165,8 +165,8 @@ class DailyReportService extends Injectable
             $row++;
         }
 
-        $sheet->setCellValue("B27", date("t"));
-        $sheet->setCellValue("B28", date("j"));
+        $sheet->setCellValue("B30", date("t"));
+        $sheet->setCellValue("B31", date("j"));
 
         $today = date('Ymd');
         $filename = BASE_DIR . "/app/logs/DailyReport-$today.xlsx";

c865dba443e35c5dac86f04a0e7cd80440a2997c
Hongshuan Li <lihsca@gmail.com>
2018-01-05 21:59:42 -0500

    change for importing project 29, 30

--- app/services/ImportService.php
+++ app/services/ImportService.php
@@ -133,11 +133,15 @@ class ImportService extends Injectable
     protected function fixValues($project, $dev, $data)
     {
         if (($project->id == 26 && $dev == 'mb-108') ||
-            ($project->id == 27 && $dev == 'mb-109')) {
+            ($project->id == 27 && $dev == 'mb-109') ||
+            ($project->id == 29 && $dev == 'mb-111') ||
+            ($project->id == 30 && $dev == 'mb-127')) {
+            // table_genmeter_ion
             $data['vln_a'] = $data['vln_ave'];
             $data['vln_b'] = $data['vln_ave'];
             $data['vln_c'] = $data['vln_ave'];
         } else if ($project->id == 28 && $dev == 'mb-011') {
+            // table_genmeter_ion_tcp
             $data['vln_a'] = $data['vln'];
             $data['vln_b'] = $data['vln'];
             $data['vln_c'] = $data['vln'];

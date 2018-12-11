7af3c52cb080f43ef5e04392fdc73f1e38a9cf02
Hongshuan L <lihsca@gmail.com>
2018-12-11 18:40:20 -0500

    browser cache the picture

--- app/controllers/PictureController.php
+++ app/controllers/PictureController.php
@@ -14,6 +14,13 @@ class PictureController extends ControllerBase
             $type = 'image/jpeg';
             header('Content-Type:'.$type);
             header('Content-Length: ' . filesize($file));
+
+            $ttl = 2592000; // 30days (60sec * 60min * 24hours * 30days)
+            $ts = gmdate("D, d M Y H:i:s", time() + $ttl) . " GMT";
+            header("Expires: $ts");
+            header("Pragma: cache");
+            header("Cache-Control: max-age=$ttl");
+
             readfile($file);
         } else {
             $this->pictureService->drawPlaceholder();

d2c7f9ef6c04d5b4e21547f870e2f4e3eeef21e7
Hongshuan L <lihsca@gmail.com>
2018-12-11 07:41:43 -0500

    new PictureController

---
+++ app/controllers/PictureController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class PictureController extends ControllerBase
+{
+    public function showAction($id)
+    {
+        $this->view->disable();
+        $picture = $this->pictureService->getPicture($id);
+
+        if ($picture) {
+            $file = $picture['filename'];
+            $type = 'image/jpeg';
+            header('Content-Type:'.$type);
+            header('Content-Length: ' . filesize($file));
+            readfile($file);
+        }
+    }
+}

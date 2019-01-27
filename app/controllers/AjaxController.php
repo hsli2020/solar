9535713eda6696248411f67dd564c8cccc86389e
Hongshuan L <lihsca@gmail.com>
2019-01-27 13:43:00 -0500

    fast import latest pictures only

--- app/controllers/AjaxController.php
+++ app/controllers/AjaxController.php
@@ -58,12 +58,23 @@ class AjaxController extends ControllerBase
         return $this->response;
     }
 
+    // Temp code for newboro4
     public function latestPicAction($prj = '')
     {
         $this->response->setContentType("application/json");
+#=>
+        // only today's files (for better performance)
+        $root = 'c:/GCS-FTP-ROOT';
+        $dir = 'NB4-Camera1/03115807_097026';
+        $today = date('Y-m-d');
+        $now = date('H/i');
 
-        $this->importService->importCameraPicture();
+        // c:/GCS-FTP-ROOT/NB4-Camera1/03115807_097026/2019-01-27/001/jpg/18/46/10[M][0@0][0].jpg
+        $folder = "$root/$dir/$today/001/jpg/$now";
 
+        // project_id = 999, camera_id=3
+        $this->importService->importPicturesInFolder($root, $folder, 999, 3);
+#=>
         $picture = $this->pictureService->getLatestPictures($prj);
         if ($picture) {
             $this->response->setJsonContent(['status' => 'OK', 'picture' => $picture ]);

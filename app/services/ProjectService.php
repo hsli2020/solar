3ff152ecc291e3013ddfb948e0b36b042d53cbea
Hongshuan L <lihsca@gmail.com>
2023-05-31 22:19:45 -0400

    fix negative kW Delivered in #52

--- app/services/ProjectService.php
+++ app/services/ProjectService.php
@@ -208,7 +208,7 @@ class ProjectService extends Injectable
                 }
             }
 
-            $details['genmeters'][$code]['kw-del'] = round($data['kwh_del']);
+            $details['genmeters'][$code]['kw-del'] = abs(round($data['kwh_del']));
             $details['genmeters'][$code]['kw-rec'] = round($data['kwh_rec']);
             $details['genmeters'][$code]['kvar'] = round($data['kva']);
             $details['genmeters'][$code]['vla'] = round($data['vln_a']);
